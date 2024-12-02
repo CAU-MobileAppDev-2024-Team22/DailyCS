3a947a4f2473df9c9a392d419fcc864e62067aa5
dev-joeysos <joeysos@naver.com>
2024-12-02 10:04:25 +0900

    [design] 로그인, 회원가입 UI 다듬기

--- app/src/main/java/com/example/firebaseexample/ui/pages/RegisterPage.kt
+++ app/src/main/java/com/example/firebaseexample/ui/pages/RegisterPage.kt
@@ -1,5 +1,6 @@
 package com.example.firebaseexample.ui.pages
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -10,24 +11,36 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import com.example.firebaseexample.ui.theme.LineColor
+import com.example.firebaseexample.ui.theme.TextFieldBorder
+import com.example.firebaseexample.ui.theme.ThemeBlue
+import com.example.firebaseexample.ui.theme.ThemeGray
+import com.example.firebaseexample.ui.theme.Typography
 import com.google.firebase.auth.FirebaseAuth
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -43,56 +56,124 @@ fun RegisterPage(
         topBar = {
             TopAppBar(
                 title = {
-                Text(text = "Register Page")
-                },
-                navigationIcon = {
-                    IconButton(onClick = {backToLoginPage()}){
-                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
-                    }
-                })
+                Text(text = "")
+                },)
+//                navigationIcon = {
+//                    IconButton(onClick = {backToLoginPage()}){
+//                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+//                    }
+//                })
         })
     { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         )
         {
+            Text(
+                text = "계정 만들기",
+                fontWeight = FontWeight.Bold,
+                style = Typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "계정으로 등록할 이메일을 하단에 입력해주세요.",
+                style = Typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = email,
                 onValueChange = {email = it},
                 label = {
-                    Text("E-mail")
-                },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                    Text(
+                        text = "E-mail",
+                        style = Typography.titleSmall,
+                        color = ThemeGray
+                    )},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                shape = RoundedCornerShape(12.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = ThemeBlue,
+                    unfocusedBorderColor = TextFieldBorder
+                )
             )
+            Spacer(modifier = Modifier.height(8.dp))
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = password,
                 onValueChange = {password = it},
                 label = {
-                    Text("Password")
-                },
+                    Text(
+                        text = "Password",
+                        style = Typography.titleSmall,
+                        color = ThemeGray
+                    )},
                 visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                shape = RoundedCornerShape(12.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = ThemeBlue,
+                    unfocusedBorderColor = TextFieldBorder
+                ),
             )
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(12.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.White,
+                ),
+                onClick = { registerUser(email, password) }
+            ){
+                Text(
+                    text = "회원가입"
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                HorizontalDivider(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(1.dp),
+                    color = LineColor
+                )
+                Text(
+                    text = "OR",
+                    modifier = Modifier.weight(1f),
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center,
+                    color = ThemeGray,
+                    style = Typography.bodySmall
+                )
+                HorizontalDivider(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(1.dp),
+                    color = LineColor
+                )
+            }
             TextButton(
-                modifier = Modifier.align(Alignment.End),
+                modifier = Modifier
+                    .fillMaxWidth(),
                 onClick = {
                     backToLoginPage()
                 }
             ) {
-                Text(text = "Go to login page")
-            }
-            Button(
-                modifier = Modifier.align(Alignment.End),
-                onClick = { registerUser(email, password) }
-            ){
-                Text(text = "Register")
+                Text(
+                    text = "로그인 페이지로 돌아가기",
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center,
+                    color = ThemeGray,
+                    style = Typography.bodyMedium,
+                    )
             }
         }
 
