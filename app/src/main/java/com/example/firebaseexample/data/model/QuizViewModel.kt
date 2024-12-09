20fe05a7a489a5ec176f382975378779b096b0cc
liljuice <juice@kakao.com>
2024-12-09 16:31:45 +0900

    [Feat] 유형별 문제 풀기: 푼 문제 count 구현

--- app/src/main/java/com/example/firebaseexample/data/model/QuizViewModel.kt
+++ app/src/main/java/com/example/firebaseexample/data/model/QuizViewModel.kt
@@ -3,6 +3,7 @@ package com.example.firebaseexample.data.model
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.firebaseexample.data.repository.QuizRepository
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -87,11 +88,23 @@ class QuizViewModel : ViewModel() {
     var brushUpCategory = mutableStateOf("")
         private set
 
+    var todayCategory = mutableStateOf("")
+        private set
+
+    var solvedCounts = mutableStateOf<Map<String, Int>>(emptyMap())
+        private set
+
     private val quizResults = mutableListOf<Map<String, Any>>()
 
     fun checkWrongAnswers() {
         viewModelScope.launch {
             isButtonEnabled.value = repository.checkWrongAnswers(this@QuizViewModel)
+        }
+    }
+
+    fun checkSolvedAnswers() {
+        viewModelScope.launch {
+            repository.checkSolvedAnswers(this@QuizViewModel)
         }
     }
 
